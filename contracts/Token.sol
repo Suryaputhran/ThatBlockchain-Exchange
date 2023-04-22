f2281d048758a30e00b662244a00923dfafebb1e
exyreams <exyreams@gmail.com>
2023-04-23 00:20:22 +0545

    Third Commit

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,10 +9,11 @@ contract Token {
     uint256 public decimals;
     uint256 public totalSupply; //1,000,000 x 10^18
 
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
     //Track Balances
     mapping(address => uint256) public balanceOf;
     //Send Tokens
-
 
     constructor(string memory _name, string memory _symbol, uint256 _decimals, uint256 _totalSupply) {
         name = _name;
@@ -22,29 +23,14 @@ contract Token {
         balanceOf[msg.sender] = totalSupply;
     }
 
-//    function
+    function transfer(address _to, uint256 _value) public returns(bool success){
+        require(balanceOf[msg.sender] >= _value, "You don't have enough balance.");
+        require(_to != address(0));
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+        balanceOf[_to] = balanceOf[_to] + _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
 
 }
-
-//contract Token {
-//    string public name = "Fenix";
-//    string public symbol = "FNX";
-//    uint256 public decimals = 18;
-//    uint256 public totalSupply = 100000000000; //1,000,000 x 10^18
-//
-//    //Track Balances
-////    mapping(address => uint256) public balanceOf;
-//    //Send Tokens
-//
-//
-////    constructor(string memory _name, string memory _symbol, uint256 _decimals, uint256 _totalSupply) {
-////        name = _name;
-////        symbol = _symbol;
-////        decimals = _decimals;
-////        totalSupply = _totalSupply * (10 ** decimals);
-////        balanceOf[msg.sender] = totalSupply;
-////    }
-//
-//    //    function
-//
-//}
--- test/Token.js
+++ test/Token.js
@@ -6,7 +6,7 @@ const tokens = (n) => {
 }
 describe("Token", () => {
 
-    let token, accounts, deployer
+    let token, accounts, deployer, receiver
 
   beforeEach(async () => {
     const Token = await ethers.getContractFactory("Token")
@@ -14,6 +14,8 @@ describe("Token", () => {
 
     accounts = await ethers.getSigners()
     deployer = accounts[0]
+    receiver = accounts[1]
+
   })
 
   describe("Deployment", () => {
@@ -45,7 +47,47 @@ describe("Token", () => {
 
   })
 
-  // describe spending...
+  describe("Sending Tokens", () => {
+      let amount, transaction, result
+
+      describe("Success", () => {
+
+          beforeEach(async () => {
+              amount = tokens(100)
+              transaction = await token.connect(deployer).transfer(receiver.address, amount)
+              result = await transaction.wait()
+          })
+
+          it("Transfers token balances.", async () => {
+              expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+              expect(await token.balanceOf(receiver.address)).to.equal(amount)
+          })
+
+          it("Emits a Transfer Event.", async () => {
+             const event = result.events[0]
+             expect(event.event).to.equal("Transfer")
+
+             const args = event.args
+             expect(args.from).to.equal(deployer.address)
+             expect(args.to).to.equal(receiver.address)
+             expect(args.value).to.equal(amount)
+          })
+      })
+
+      describe("Failure", () => {
+          it("Rejects insufficient balances.", async () => {
+              const invalidAmount = tokens(100000000)
+              await expect(token.connect(deployer).transfer(receiver.address, invalidAmount))
+          })
+
+          it('rejects invalid recipent', async () => {
+              const amount = tokens(100)
+              await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+          })
+      })
+
+
+  })
 
   //describe approving...
 
