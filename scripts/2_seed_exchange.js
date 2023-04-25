49e656aa9c933a5529cd98ad2f4c4961c56c1a0e
exyreams <exyreams@gmail.com>
2023-04-25 21:00:26 +0545

    Eleventh Commit

--- scripts/2_seed_exchange.js
+++ scripts/2_seed_exchange.js
@@ -11,7 +11,11 @@ const wait = (seconds) => {
 async function main() {
 
     // Fetch accounts from wallet - these are unlocked
-    const accounts = await ethers.getSigners();
+    const accounts = await ethers.getSigners()
+
+    // Fetch the deployed contract or tokens
+    const Finix = await ethers.getContractAt("Token", "0x0165878A594ca255338adfa4d48449f69242Eb8F")
+    console.log(`Token fetched: ${Finix.address}`)
 
     const Auriga = await ethers.getContractAt("Token", "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9")
     console.log(`Token fetched: ${Auriga.address}`)
@@ -19,31 +23,29 @@ async function main() {
     const Empyrean = await ethers.getContractAt("Token", "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707")
     console.log(`Token fetched: ${Empyrean.address}`)
 
-    const Finix = await ethers.getContractAt("Token", "0x0165878A594ca255338adfa4d48449f69242Eb8F")
-    console.log(`Token fetched: ${Finix.address}`)
-
-    const Helix = await ethers.getContractAt("Token", "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853")
-    console.log(`Token fetched: ${Helix.address}`)
-
-    const Quantum = await ethers.getContractAt("Token", "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6")
-    console.log(`Token fetched: ${Quantum.address}`)
-
-    const Sirius = await ethers.getContractAt("Token", "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318")
-    console.log(`Token fetched: ${Sirius.address}`)
-
-    const Zeroconium = await ethers.getContractAt("Token", "0x610178dA211FEF7D417bC0e6FeD39F05609AD788")
-    console.log(`Token fetched: ${Zeroconium.address}`)
+    //
+    // const Helix = await ethers.getContractAt("Token", "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853")
+    // console.log(`Token fetched: ${Helix.address}`)
+    //
+    // const Quantum = await ethers.getContractAt("Token", "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6")
+    // console.log(`Token fetched: ${Quantum.address}`)
+    //
+    // const Sirius = await ethers.getContractAt("Token", "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318")
+    // console.log(`Token fetched: ${Sirius.address}`)
+    //
+    // const Zeroconium = await ethers.getContractAt("Token", "0x610178dA211FEF7D417bC0e6FeD39F05609AD788")
+    // console.log(`Token fetched: ${Zeroconium.address}`)
 
     //Fetch the deployed DecentralizedExchange
     const decentralizedexchange = await ethers.getContractAt("DecentralizedExchange", "0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e")
-    console.log(`Token fetched: ${decentralizedexchange.address}`)
+    console.log(`Decentralized Exchange fetched: ${decentralizedexchange.address}\n`)
 
     //Give tokens to account[1]
     const sender = accounts[0]
     const receiver = accounts[1]
     let amount = tokens(10000)
 
-    //user1 transfers 10,000
+    //user1 transfers 10,000 Auriga
     let transaction, result
     transaction = await Auriga.connect(sender).transfer(receiver.address, amount)
     console.log(`Transferred ${amount} tokens from ${sender.address} to ${receiver.address}\n`)
@@ -58,10 +60,11 @@ async function main() {
     await transaction.wait()
     console.log(`Approved ${amount} tokens from ${user1.address}`)
 
+
     //User1 deposit 10,000 Finix tokens
     transaction = await decentralizedexchange.connect(user1).depositToken(Finix.address, amount)
     await transaction.wait()
-    console.log(`Deposited ${amount} tokens from ${user1.address}\n`)
+    console.log(`Deposited ${amount} Ether from ${user1.address}\n`)
 
     //User2 approves 10,000 Auriga tokens
     transaction = await Auriga.connect(user2).approve(decentralizedexchange.address, amount)
@@ -74,77 +77,89 @@ async function main() {
     console.log(`Deposited ${amount} tokens from ${user2.address}\n`)
 
     //Make & Cancel Orders
+
     //user1 makes order to get tokens
     let orderId
-    transaction = await decentralizedexchange.connect(user1).makeOrder(Finix.address, tokens(10), Auriga.address, tokens(55))
-    await transaction.wait()
-    console.log(`Made Order from ${user1.address}`)
+    transaction = await decentralizedexchange.connect(user1).makeOrder(Auriga.address, tokens(100), Finix.address, tokens(10))
+    result = await transaction.wait()
+    console.log(`Made order from ${user1.address}`)
 
     //Cancel Orders
+
     //user1 cancels order
-    orderId = result.events[0].args.orderId
+    orderId = result.events[0].args.id
     transaction = await decentralizedexchange.connect(user1).cancelOrder(orderId)
-    await transaction.wait()
-    console.log(`Cancelled Order from ${user1.address}\n`)
+    result = await transaction.wait()
+    console.log(`Cancelled order from ${user1.address}\n`)
 
     //wait 1 seconds
     await wait(1)
 
     //Fill Orders
+
     //user1 makes order to get tokens
-    transaction = await decentralizedexchange.connect(user1).makeOrder(Finix.address, tokens(20), Auriga.address, tokens(70))
-    await transaction.wait()
-    console.log(`Made Order from ${user1.address}`)
+    transaction = await decentralizedexchange.connect(user1).makeOrder(Auriga.address, tokens(100), Finix.address, tokens(10))
+    result = await transaction.wait()
+    console.log(`Made order from ${user1.address}`)
 
     //user2 fills order
-    orderId = result.events[0].args.orderId
+    orderId = result.events[0].args.id
     transaction = await decentralizedexchange.connect(user2).fillOrder(orderId)
     result = await transaction.wait()
-    console.log(`Filled Order from ${user1.address}\n`)
+    console.log(`Filled order from ${user1.address}\n`)
 
     //wait 1 seconds
     await wait(1)
 
     //user1 makes another order
-    transaction = await decentralizedexchange.connect(user1).makeOrder(Finix.address, tokens(16), Auriga.address, tokens(23))
-    await transaction.wait()
-    console.log(`Made Order from ${user1.address}`)
+    transaction = await decentralizedexchange.makeOrder(Auriga.address, tokens(50), Finix.address, tokens(15))
+    result = await transaction.wait()
+    console.log(`Made order from ${user1.address}`)
 
-    //user2 fills the order again
-    orderId = result.events[0].args.orderId
+    //user2 fills the another order
+    orderId = result.events[0].args.id
     transaction = await decentralizedexchange.connect(user2).fillOrder(orderId)
     result = await transaction.wait()
-    console.log(`Filled Order from ${user1.address}\n`)
+    console.log(`Filled order from ${user1.address}\n`)
 
     //wait 1 seconds
     await wait(1)
 
-    //users2 fills final order
-    orderId = result.events[0].args.orderId
+    //users1 makes final order
+    transaction = await decentralizedexchange.connect(user1).makeOrder(Auriga.address, tokens(200), Finix.address, tokens(20))
+    result = await transaction.wait()
+    console.log(`Made order from ${user1.address}`)
+
+    //user2 fills final order
+    orderId = result.events[0].args.id
     transaction = await decentralizedexchange.connect(user2).fillOrder(orderId)
     result = await transaction.wait()
-    console.log(`Filled Order from ${user1.address}\n`)
+    console.log(`Filled order from ${user1.address}\n`)
 
     //wait 1 seconds
     await wait(1)
 
-    /////////////////////////////////////
     //Seed open orders
 
-    //user1 makes orders
+    //user1 makes 10 orders
     for (let i = 1; i <= 10; i++) {
-        transaction = await decentralizedexchange.connect(user1).makeorder(Finix.address, tokens(10 * i), Auriga.address, tokens(20))
+        transaction = await decentralizedexchange.connect(user1).makeOrder(Auriga.address, tokens(10 * i), Finix.address, tokens(10))
         result = await transaction.wait()
-        console.log(`Made Order from ${user1.address}`)
-        //wait 1  seconds
+
+        console.log(`Made order from ${user1.address}`)
+
+        // Wait 1 second
         await wait(1)
     }
 
+    //user2 makes 10 orders
     for (let i = 1; i <= 10; i++) {
-        transaction = await decentralizedexchange.connect(user2).makeorder(Auriga.address, tokens(10), Finix.address, tokens(20 * i))
+        transaction = await decentralizedexchange.connect(user2).makeOrder(Finix.address, tokens(10), Auriga.address, tokens(10 * i))
         result = await transaction.wait()
-        console.log(`Made Order from ${user2.address}`)
-        //wait 1  seconds
+
+        console.log(`Made order from ${user2.address}`)
+
+        // Wait 1 second
         await wait(1)
     }
 
