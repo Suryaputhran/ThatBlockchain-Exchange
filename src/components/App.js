1721e4d12f13c69ee99f956b40f1f7aac611d55b
exyreams <exyreams@gmail.com>
2023-04-27 21:10:03 +0545

    Fourteenth Commit

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import "../App.css";
+function App() {
+
+    const loadBlockchainData = async () => {
+        const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+        console.log(accounts[0])
+    }
+
+    return (
+        <div>
+
+            {/* Navbar */}
+
+            <main className='exchange grid'>
+                <section className='exchange__section--left grid'>
+
+                    {/* Markets */}
+
+                    {/* Balance */}
+
+                    {/* Order */}
+
+                </section>
+                <section className='exchange__section--right grid'>
+
+                    {/* PriceChart */}
+
+                    {/* Transactions */}
+
+                    {/* Trades */}
+
+                    {/* OrderBook */}
+
+                </section>
+            </main>
+
+            {/* Alert */}
+
+        </div>
+    );
+}
+
+export default App;
