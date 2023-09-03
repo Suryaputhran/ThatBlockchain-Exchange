c15856f8ac09a699a035c8c5b5166a892552c896
Pranoy Manu K.M <pranoysunil24@gmail.com>
2023-09-03 18:09:40 +0000

    t

--- hardhat.config.js
+++ hardhat.config.js
@@ -37,7 +37,7 @@ module.exports = {
       accounts: [privateKeys] 
     },
     mumbai: {
-      url: 'https://rpc-mumbai.maticvigil.com',
+      url: "https://polygon-rpc.com",
       accounts: '4ff581f219bafcf5af91d52e1a8b484fd93123a475437ee96d293ff423338632'
     
     }
